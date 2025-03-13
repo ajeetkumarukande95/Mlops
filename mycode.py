74f8e6a7c6fba37f32f90df0536b9ba0d873be42
ajeetkumarukande <ajeetkumarukande95@gmail.com>
2025-03-13 22:21:30 +0530

    third version of data saved

--- mycode.py
+++ mycode.py
@@ -13,6 +13,8 @@ new_row_loc = {'Name':'GF1','Age':20,"City":'City1'}
 
 df.loc[len(df.index)] = new_row_loc
 
+new_row_loc2 = {'Name':"GF2",'Age':30,'City':"city2"}
+df.loc[len(df.index)] = new_row_loc2
 
 
 data_dir = "data"
